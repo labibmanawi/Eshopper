ec0c25c3d73a293aaf35996b3b583d0711c2c3ef
Labib Manawi <zacklaviev@gmail.com>
2023-03-17 14:57:42 +0700

    first commit

---
+++ database/migrations/2023_01_19_040115_checkout.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_checkout', function(Blueprint $table) {
+            $table->increments('id_checkout');
+            $table->string('id_user');
+            $table->float('total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
